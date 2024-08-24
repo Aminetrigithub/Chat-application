c0d6747d78a90fbfca790cdf7b7807c9d01ea6bc
Amine Trigui <aminetrigui.github@gmail.com>
2024-08-24 17:12:29 +0100

    Auth UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import { Button } from './components/ui/button'
-
-const App = () => {
+import './App.css'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import Auth from './pages/auth'
+import Chat from './pages/chat'
+import Profile from './pages/profile'
+function App() {
   return (
-    <Button> Click</Button>
+    <>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/auth" element={<Auth />}/>
+        <Route path="/chat" element={<Chat />}/>
+        <Route path="/profile" element={<Profile />}/>
+        <Route path="*" element={<Navigate to="/auth" />}/>
+      </Routes>
+      </BrowserRouter>
+    </>
   )
 }
-
 export default App
--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -1,7 +1,62 @@
-
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import Victory from "../../assets/victory.svg";
+import Background from "../../assets/login2.png";
+import { TabsContent } from "@radix-ui/react-tabs";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
 const Auth = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+
+  const handleLogin  = async () => { }
+  const handleSignUp = async () => { }
   return (
-    <div>Auth</div>
-  )
-}
-export default Auth
+    <div className="h-[100vh] w-[100vw] flex items-center justify-center">
+      <div className="h-[80vh] w-[80vw] bg-white border-2 border-white text-opacity-90 shadow-2xl md:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl grid xl:grid-cols-2">
+        <div className="flex flex-col gap-10 items-center justify-center">
+          <div className="flex flex-col  items-center justify-center">
+          <div className="flex items-center justify-center">
+            <h1 className="text-5xl font-bold md:text-6xl ">Welcome</h1>
+            <img src={Victory} className="h-[100px]" />
+          </div>
+          <p className="font-medium text-center">
+            Fill in the details to get started with the best chat application
+          </p>
+        </div>
+        <div className="flex item-center justify-center w-full">
+          <Tabs className="w-3/4"> 
+            <TabsList className="bg-transparent rounded-none w-full">
+              <TabsTrigger value="login" className="data-[state=active]:bg-transparent text-black 
+              text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:font-semibold data-[state=active]:border-b-purple-500
+              p-3 transition-all duration-300" >Login</TabsTrigger>
+              <TabsTrigger value="signUp" className="data-[state=active]:bg-transparent text-black 
+              text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:font-semibold data-[state=active]:border-b-purple-500
+              p-3 transition-all duration-300">SignUp</TabsTrigger>
+            </TabsList>
+            <TabsContent className="flex flex-col gap-5 mt-10" value="login">
+              <Input placeholder="Email" type="email" className="rounded-full p-6"  /> 
+              <Input placeholder="Password" type="password" className="rounded-full p-6" /> 
+              <Button className="flex flex-col gap-5 " onClick={handleLogin}>Login</Button>
+            </TabsContent>
+            <TabsContent className="flex flex-col gap-5 " value="signUp">
+            <Input placeholder="Email" type="email" className="rounded-full p-6" value={email} onChange={(e) => setEmail(e.target.value)}  /> 
+            <Input placeholder="Password" type="password" className="rounded-full p-6" value={password} onChange={(e) => setPassword(e.target.value)} /> 
+            <Input placeholder="Confirm Password" type="confirm password" className="rounded-full p-6" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} /> 
+            <Button className="flex flex-col gap-5 " onClick={handleSignUp} >SignUp</Button>
+            </TabsContent>
+          </Tabs>
+        </div>
+      </div>
+      <div className=" hidden xl:flex items-center justify-center" >
+         <img src={Background} className="h-[700px]" />
+      </div>
+    </div>
+  </div>
+  );
+};
+
+export default Auth;
